05bc6f757f9439162b610d8bb6cb70b67e1faac7
RAVULAPALLI KOMALI LAKSHMI PADMAVATHI <104910204+VU21CSEN0101454@users.noreply.github.com>
2024-06-28 15:58:58 +0530

    Update urls.py

--- SkillProject/SkillProject/urls.py
+++ SkillProject/SkillProject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('courses/', include('courses.urls')),
-    path('', include('django.contrib.auth.urls')),
-    path('', include('SkillDevelopmentPlatform.views.home'), name='home'),
+    path('', home, name='home'),
 ]
+
