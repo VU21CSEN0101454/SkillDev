de9295d37e6493d61294fb34c078a0d43525c208
RAVULAPALLI KOMALI LAKSHMI PADMAVATHI <104910204+VU21CSEN0101454@users.noreply.github.com>
2024-06-28 16:21:07 +0530

    Update settings.py

--- SkillProject/SkillProject/settings.py
+++ SkillProject/SkillProject/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.233.120.20','localhost','0.0.0.0']
+ALLOWED_HOSTS = ['13.233.120.20','localhost','0.0.0.0:8000']
 
 
 # Application definition
